126b3d49f42b1cdf171dd2ffa8667792d3672eec
mayurpatel141 <mayurpatel141@gmail.com>
2018-11-03 18:14:02 +0530

    getting started with test scenarios

--- src/com/stta/SuiteOne/SuiteOneCaseOne.java
+++ src/com/stta/SuiteOne/SuiteOneCaseOne.java
@@ -2,11 +2,17 @@ package com.stta.SuiteOne;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 import com.stta.utility.Read_XLS;
 import com.stta.utility.SuiteUtility;
 
@@ -19,14 +25,16 @@ public class SuiteOneCaseOne extends SuiteOneBase{
 	String ToRunColumnNameTestCase = null;
 	String ToRunColumnNameTestData = null;
 	String TestDataToRun[]=null;
+	static boolean TestCasePass=true;
 	static int DataSet=-1;	
 	static boolean Testskip=false;
 	static boolean Testfail=false;
+	SoftAssert s_assert =null;	
 	
 	@BeforeTest
 	public void checkCaseToRun() throws IOException{
 		//Called init() function from SuiteBase class to Initialize .xls Files
-		init();	
+		init();			
 		//To set SuiteOne.xls file's path In FilePath Variable.
 		FilePath = TestCaseListExcelOne;		
 		TestCaseName = this.getClass().getSimpleName();	
@@ -36,10 +44,16 @@ public class SuiteOneCaseOne extends SuiteOneBase{
 		ToRunColumnNameTestCase = "CaseToRun";
 		//Name of column In Test Case Data sheets.
 		ToRunColumnNameTestData = "DataToRun";
+		//Bellow given syntax will Insert log In applog.log file.
+		Add_Log.info(TestCaseName+" : Execution started.");
 		
 		//To check test case's CaseToRun = Y or N In related excel sheet.
 		//If CaseToRun = N or blank, Test case will skip execution. Else It will be executed.
-		if(!SuiteUtility.checkToRunUtility(FilePath, SheetName,ToRunColumnNameTestCase,TestCaseName)){			
+		if(!SuiteUtility.checkToRunUtility(FilePath, SheetName,ToRunColumnNameTestCase,TestCaseName)){
+			Add_Log.info(TestCaseName+" : CaseToRun = N for So Skipping Execution.");
+			//To report result as skip for test cases In TestCasesList sheet.
+			SuiteUtility.WriteResultUtility(FilePath, SheetName, "Pass/Fail/Skip", TestCaseName, "SKIP");
+			//To throw skip exception for this test case.
 			throw new SkipException(TestCaseName+"'s CaseToRun Flag Is 'N' Or Blank. So Skipping Execution Of "+TestCaseName);
 		}	
 		//To retrieve DataToRun flags of all data set lines from related test data sheet.
@@ -51,38 +65,92 @@ public class SuiteOneCaseOne extends SuiteOneBase{
 	public void SuiteOneCaseOneTest(String Departure_airport,String Arrival_airport){
 		
 		DataSet++;
+		
+		//Created object of testng SoftAssert class.
+		s_assert = new SoftAssert();
+		
 		//If found DataToRun = "N" for data set then execution will be skipped for that data set.
-		if(!TestDataToRun[DataSet].equalsIgnoreCase("Y")){			
+		if(!TestDataToRun[DataSet].equalsIgnoreCase("Y")){	
+			Add_Log.info(TestCaseName+" : DataToRun = N for data set line "+(DataSet+1)+" So skipping Its execution.");
+			//If DataToRun = "N", Set Testskip=true.
+			Testskip=true;
 			throw new SkipException("DataToRun for row number "+DataSet+" Is No Or Blank. So Skipping Its Execution.");
 		}
 		
 		//If found DataToRun = "Y" for data set then bellow given lines will be executed.
-		System.out.println("Value Of DataCol1 = "+Departure_airport);
-		System.out.println("Value Of DataCol2 = "+Arrival_airport);		
-	}	
-	
+		//To Convert data from String to Integer
+		String ValueOne = Departure_airport;
+		String ValueTwo = Arrival_airport;
+		
+		
+		//To Initialize Chrome browser.
+		loadWebBrowser();
+		//To navigate to URL.
+		driver.navigate().to("https://www.emirates.com/ae/english/");
+		driver.navigate().refresh();
+		
+		WebElement depatureAirport= driver.findElement(By.xpath("//input[@name='Departure airport']"));
+		depatureAirport.sendKeys(ValueOne);
+		depatureAirport.sendKeys(Keys.TAB);
+		WebElement arrivalAirport= driver.findElement(By.xpath("//input[@name='Arrival airport']"));
+		arrivalAirport.sendKeys(ValueTwo);
+		arrivalAirport.sendKeys(Keys.TAB);
+		WebElement searchFlights= driver.findElement(By.xpath("//form/button/span"));
+		searchFlights.click();
+		
+		
+		if(Testfail){
+			//At last, test data assertion failure will be reported In testNG reports and It will mark your test data, test case and test suite as fail.
+			s_assert.assertAll();		
+		}
+	}
 	
 	//@AfterMethod method will be executed after execution of @Test method every time.
-		@AfterMethod
-		public void reporterDataResults(){		
-			if(Testskip)
-				//If found Testskip = true, Result will be reported as SKIP against data set line In excel sheet.
-				SuiteUtility.WriteResultUtility(FilePath, TestCaseName, "Pass/Fail/Skip", DataSet+1, "SKIP");
-			else if(Testfail){
-				//If found Testfail = true, Result will be reported as FAIL against data set line In excel sheet.
-				SuiteUtility.WriteResultUtility(FilePath, TestCaseName, "Pass/Fail/Skip", DataSet+1, "FAIL");			
-			}else
-				//If found Testskip = false and Testfail = false, Result will be reported as PASS against data set line In excel sheet.
-				SuiteUtility.WriteResultUtility(FilePath, TestCaseName, "Pass/Fail/Skip", DataSet+1, "PASS");
-			//At last make both flags as false for next data set.
-			Testskip=false;
-			Testfail=false;
+	@AfterMethod
+	public void reporterDataResults(){		
+		if(Testskip){
+			Add_Log.info(TestCaseName+" : Reporting test data set line "+(DataSet+1)+" as SKIP In excel.");
+			//If found Testskip = true, Result will be reported as SKIP against data set line In excel sheet.
+			SuiteUtility.WriteResultUtility(FilePath, TestCaseName, "Pass/Fail/Skip", DataSet+1, "SKIP");
 		}
-	//This data provider method will return 2 column's data one by one In every Iteration.
+		else if(Testfail){
+			Add_Log.info(TestCaseName+" : Reporting test data set line "+(DataSet+1)+" as FAIL In excel.");
+			//To make object reference null after reporting In report.
+			s_assert = null;
+			//Set TestCasePass = false to report test case as fail In excel sheet.
+			TestCasePass=false;	
+			//If found Testfail = true, Result will be reported as FAIL against data set line In excel sheet.
+			SuiteUtility.WriteResultUtility(FilePath, TestCaseName, "Pass/Fail/Skip", DataSet+1, "FAIL");			
+		}else{
+			Add_Log.info(TestCaseName+" : Reporting test data set line "+(DataSet+1)+" as PASS In excel.");
+			//If found Testskip = false and Testfail = false, Result will be reported as PASS against data set line In excel sheet.
+			SuiteUtility.WriteResultUtility(FilePath, TestCaseName, "Pass/Fail/Skip", DataSet+1, "PASS");
+		}
+		//At last make both flags as false for next data set.
+		Testskip=false;
+		Testfail=false;
+	}
+	
+	//This data provider method will return 4 column's data one by one In every Iteration.
 	@DataProvider
 	public Object[][] SuiteOneCaseOneData(){
-		//To retrieve data from Data 1 Column,Data 2 Column of SuiteOneCaseOne data Sheet.
+		//To retrieve data from Data 1 Column,Data 2 Column,Data 3 Column and Expected Result column of SuiteOneCaseOne data Sheet.
 		//Last two columns (DataToRun and Pass/Fail/Skip) are Ignored programatically when reading test data.
 		return SuiteUtility.GetTestDataUtility(FilePath, TestCaseName);
+	}	
+	
+	//To report result as pass or fail for test cases In TestCasesList sheet.
+	@AfterTest
+	public void closeBrowser(){
+		//To Close the web browser at the end of test.
+		closeWebBrowser();
+		if(TestCasePass){
+			Add_Log.info(TestCaseName+" : Reporting test case as PASS In excel.");
+			SuiteUtility.WriteResultUtility(FilePath, SheetName, "Pass/Fail/Skip", TestCaseName, "PASS");
+		}
+		else{
+			Add_Log.info(TestCaseName+" : Reporting test case as FAIL In excel.");
+			SuiteUtility.WriteResultUtility(FilePath, SheetName, "Pass/Fail/Skip", TestCaseName, "FAIL");			
+		}
 	}
 }
--- src/com/stta/utility/Read_XLS.java
+++ src/com/stta/utility/Read_XLS.java
@@ -2,13 +2,12 @@ package com.stta.utility;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Cell;
 
 
 
